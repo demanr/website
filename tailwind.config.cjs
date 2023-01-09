aac94b17f9031cc7c122d7af37b233c4cfb2cd04
Rachelle DeMan <demanr@mcmaster.ca>
2023-01-09 16:12:19 -0500

    feat: added button and link animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		fontFamily: {
-			sans: ['Red Hat Display', 'sans-serif'],
-		}
-		,
-		extend: {
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["Red Hat Display", "Inter", "sans-serif"],
+    },
+    extend: {
+      keyframes: {
+        "fade": {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+        animation: {
+          'fade-in-s': 'fade 4.5s ease-in-out'
+        }
+      },
+    },
+	variants: {},
+    plugins: [],
+  },
+};
